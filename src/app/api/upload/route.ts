79162d186ea63398b0aaa67e48759232cae8fbfb
CaseyDevs <CaseyDevs5@gmail.com>
2025-05-01 21:47:01 +0100

    Create upload route

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+import { v4 as uuid } from "uuid";
+
+// Create a Supabase client
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+
+    const file:File | null = formData.get("file") as unknown as File;
+    const title = formData.get("title") as string;
+    const artist = formData.get("artist") as string;
+    const genre = formData.get("genre") as string;
+    const bpm = formData.get("bpm") as string;
+    const coverImg = formData.get("coverImg") as string;
+
+    if (!file || !title || !artist || !genre || !bpm || !coverImg) {
+      return NextResponse.json({ error: "Missing Fields" }, { status: 400 });
+    }
+
+    // Generate a unique filename
+    const fileName = `${uuid()}-${file.name}`;
+
+    // Upload the file to Supabase Storage
+    const { data, error } = await supabase.storage
+      .from("beats")
+      .upload(fileName, file);
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json({ message: "File uploaded successfully", data });
+    
+  } catch (error) {
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import NavBar from "../components/NavBar/NavBar";
 import { FaCloudUploadAlt } from "react-icons/fa";
 
