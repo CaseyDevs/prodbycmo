51c13679c714571f48ba0988220004979f551f57
CaseyDevs <CaseyDevs5@gmail.com>
2025-05-01 11:49:02 +0100

    Update route

--- src/app/api/songs/route.ts
+++ src/app/api/songs/route.ts
@@ -2,8 +2,8 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const songs = await prisma.beat.findMany();
-  return NextResponse.json(songs);
+  const beats = await prisma.beat.findMany();
+  return NextResponse.json(beats);
 }
 
 export async function POST(req: Request) {
