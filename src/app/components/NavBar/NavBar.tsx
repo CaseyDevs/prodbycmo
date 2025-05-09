bbaadb812f91fa6d3bdcec95748829055325783b
Casey Page <CaseyDevs5@gmail.com>
2025-05-09 22:29:48 +0100

    Homepage logo

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -10,17 +10,17 @@ export default function NavBar() {
     ];
 
     return (
-        <nav className="flex items-center justify-between p-4 bg-zinc-900 text-white">
-        <div className="text-lg font-bold">Logo</div>
-        <ul className="flex space-x-4">
-            {items.map((items, index) => (
-                <li key={index}>
-                    <Link href={items.href} className="hover:text-zinc-400 transition-colors">
-                        {items.label}
-                    </Link>
-                </li>
-            ))}
-        </ul>
+        <nav className="flex items-center justify-between p-4 bg-black-100 text-white">
+            <div className="text-lg font-bold">ProdByCmo</div>
+            <ul className="flex space-x-4">
+                {items.map((items, index) => (
+                    <li key={index}>
+                        <Link href={items.href} className="hover:text-zinc-400 transition-colors">
+                            {items.label}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </nav>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import NavBar from "./components/NavBar/NavBar";
 export default function Home() {
   return (
     <>
-    <NavBar />
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-      <p className="mt-4 text-lg">Welcome to my Next.js app!</p>
-    </main>
+      <NavBar />
+      <img src="https://59yoapebrc.ufs.sh/f/V2i90s9zI34cakFCTTsOicRW3Hu7ZzGmF58VAQhK126rn9YP" alt="" className="m-auto h-48 w-108" />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p className="mt-4 text-lg">Ignore</p>
+      </main>
     </>
   );
 }
