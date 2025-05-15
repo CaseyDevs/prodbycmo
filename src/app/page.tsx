6702f21c67a6958e6de618ce071b51ed567da00a
CaseyDevs <CaseyDevs5@gmail.com>
2025-05-15 18:44:57 +0100

    Home page adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar/NavBar";
 import Beat from "./components/Beats/Beat";
+import Link from "next/link";
 import { getFeaturedBeats } from "@/lib/queries";
 
 export default async function Home() {
@@ -10,25 +11,27 @@ export default async function Home() {
     <>
       <NavBar />
       <main className="flex min-h-screen flex-col items-center p-24">
+        {/* SEARCH BAR HERE */}
         <h1 className="text-2xl font-bold text-center">Featured: </h1>
         <div className="flex flex-col items-center">
           <div className="space-y-6 my-6">
-                    {featuredBeats.map((beat) => (
-                      <Beat
-                        key={beat.id}
-                        beat={{
-                          id: beat.id.toString(),
-                          title: beat.title,
-                          artist: beat.artists.map((a) => a.name).join(", "),
-                          genre: beat.genre,
-                          bpm: beat.bpm,
-                          key: beat.key,
-                          coverImg: beat.coverImg,
-                          url: beat.url,
-                        }}
-                      />
-                    ))}
-                  </div>
+            {featuredBeats.map((beat) => (
+              <Beat
+                key={beat.id}
+                beat={{
+                    id: beat.id.toString(),
+                    title: beat.title,
+                    artist: beat.artists.map((a) => a.name).join(", "),
+                    genre: beat.genre,
+                    bpm: beat.bpm,
+                    key: beat.key,
+                    coverImg: beat.coverImg,
+                    url: beat.url,
+                }}
+              />
+            ))}
+          </div>
+          <p>Search through my entire catalog <Link href={"/beats"} className="text-blue-400 hover:opacity-80 transition-colors">here...</Link></p>
         </div>
       </main>
     </>
