f3f8633ef42dd0b444e07b94b30535be45edcd99
CaseyDevs <CaseyDevs5@gmail.com>
2025-05-15 18:34:02 +0100

    Add getBeats func to lib dir

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,5 +1,13 @@
 import { prisma } from "@/lib/prisma"
 
+export async function getBeats() {
+    return prisma.beat.findMany({
+        include: {
+          artists: true,
+        },
+      });
+}
+
 export async function getFeaturedBeats() {
     return prisma.beat.findMany({
         where: { featured: true }, 
