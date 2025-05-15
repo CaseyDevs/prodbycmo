4419cc89968606614db603c1b74019517f34328f
CaseyDevs <CaseyDevs5@gmail.com>
2025-05-15 18:09:00 +0100

    Display featured beats on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-
 import NavBar from "./components/NavBar/NavBar";
+import { prisma } from "@/lib/prisma";
+import Beat from "./components/Beats/Beat";
 
-export default function Home() {
+export default async function Home() {
+
+  const featuredBeats = await prisma.beat.findMany({
+    where: {
+      featured: true,
+    }, include: {
+      artists: true,
+    },
+  });
+
   return (
     <>
       <NavBar />
@@ -9,7 +19,23 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-center">Featured: </h1>
         <div className="flex flex-col items-center">
           {/* Featured beat here */}
-
+          <div className="space-y-6">
+                    {featuredBeats.map((beat) => (
+                      <Beat
+                        key={beat.id}
+                        beat={{
+                          id: beat.id.toString(),
+                          title: beat.title,
+                          artist: beat.artists.map((a) => a.name).join(", "),
+                          genre: beat.genre,
+                          bpm: beat.bpm,
+                          key: beat.key,
+                          coverImg: beat.coverImg,
+                          url: beat.url,
+                        }}
+                      />
+                    ))}
+                  </div>
         </div>
       </main>
     </>
