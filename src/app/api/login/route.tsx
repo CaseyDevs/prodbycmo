c187af0cc858d3686e82c77f32ebe9282fcc25ec
CaseyDevs <CaseyDevs5@gmail.com>
2025-05-16 15:13:18 +0100

    Remove supabase client from login route

--- src/app/api/login/route.tsx
+++ src/app/api/login/route.tsx
@@ -1,15 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
 import { v4 as uuid } from "uuid";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcrypt";
-
-
-// Create a Supabase client
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
 
 export async function POST(request: NextRequest) {
     try {
