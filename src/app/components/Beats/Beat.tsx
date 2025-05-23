468b15170aeebf0b229bf40e93f261a26b3e9b03
Casey Page <CaseyDevs5@gmail.com>
2025-05-23 21:47:38 +0100

    Update metadata

--- src/app/components/Beats/Beat.tsx
+++ src/app/components/Beats/Beat.tsx
@@ -14,7 +14,7 @@ export default function Beat({ beat }: { beat: { id: string; title: string; arti
 		if (!audio) return;
 
 		audioRefs.push(audio);
-		
+
 		const handlePlay = () => {
 			audioRefs.forEach((otherAudio) => {
 				if (otherAudio !== audio) {
@@ -37,31 +37,31 @@ export default function Beat({ beat }: { beat: { id: string; title: string; arti
 	}, []);
 
 
-    return (
-			<div className="flex gap-6 items-center bg-zinc-800 p-4 rounded-lg">
-				<img
-					src={beat.coverImg}
-					alt="Beat Cover"
-					className="w-18 h-18 rounded-lg"
-				/>
-				<div>
-					<h2 className="text-xl font-bold">{beat.title}</h2>
-					<p className="text-sm text-gray-400">Artists: {beat.artist}</p>
-					<div className="flex flex-row gap-2">
-						<p className="text-sm text-gray-400"><strong>Genre:</strong> {beat.genre}</p>
-						<p className="text-sm text-gray-400"><strong>BPM:</strong> {beat.bpm}</p>
-						<p className="text-sm text-gray-400"><strong>Key:</strong> {beat.key}</p>
-						<audio
-							ref={audioRef}
-							controls
-							preload="metadata"
-							className="ml-4 h-8 [&::-webkit-media-controls-panel]:bg-zinc-700 [&::-webkit-media-controls-current-time-display]:text-white [&::-webkit-media-controls-time-remaining-display]:text-white [&::-webkit-media-controls-timeline]:bg-zinc-600 [&::-webkit-media-controls-play-button]:text-white [&::-webkit-media-controls-timeline]:rounded-lg"
-						>
-							<source src={beat.url} />
-							Your browser does not support the audio element.
-						</audio>
-					</div>
+	return (
+		<div className="flex gap-6 items-center bg-zinc-800 p-4 rounded-lg">
+			<img
+				src={beat.coverImg}
+				alt="Beat Cover"
+				className="w-18 h-18 rounded-lg"
+			/>
+			<div>
+				<h2 className="text-xl font-bold">{beat.title}</h2>
+				<p className="text-sm text-gray-400">Artists: {beat.artist}</p>
+				<div className="flex flex-row gap-2">
+					<p className="text-sm text-gray-400"><strong>Genre:</strong> {beat.genre}</p>
+					<p className="text-sm text-gray-400"><strong>BPM:</strong> {beat.bpm}</p>
+					<p className="text-sm text-gray-400"><strong>Key:</strong> {beat.key}</p>
+					<audio
+						ref={audioRef}
+						controls
+						preload="metadata"
+						className="ml-4 h-8 [&::-webkit-media-controls-panel]:bg-zinc-700 [&::-webkit-media-controls-current-time-display]:text-white [&::-webkit-media-controls-time-remaining-display]:text-white [&::-webkit-media-controls-timeline]:bg-zinc-600 [&::-webkit-media-controls-play-button]:text-white [&::-webkit-media-controls-timeline]:rounded-lg"
+					>
+						<source src={beat.url} />
+						Your browser does not support the audio element.
+					</audio>
 				</div>
 			</div>
-  );
+		</div>
+	);
 }
