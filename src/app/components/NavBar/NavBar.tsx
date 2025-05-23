749726b58632bcc40e4577cbe8da4289cf12d409
Casey Page <CaseyDevs5@gmail.com>
2025-05-23 20:25:58 +0100

    Handle signout function

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
 
 
@@ -25,13 +24,30 @@ export default function NavBar() {
     }, [])
 
     const items = [
-        ...(role ? [{ href: "/signout", label: "Sign Out" }] : [{ href: "/login", label: "Login" }]),
+        ...(role ? [{ href: "/", label: "Sign Out" }] : [{ href: "/login", label: "Login" }]),
         { href: "/beats", label: "Beats" },
         { href: "/contact", label: "Contact" },
     ];
 
     if (role === "ADMIN") {
         items.push({ href: "/upload", label: "Upload" });
+    }
+
+    // Call signout API when the user clicks the "Sign Out" link
+    const handleSignout = async () => {
+        const response = await fetch("/api/signout", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            console.log("Sign out successful");
+            setRole(null);
+        } else {
+            console.error("Sign out failed");
+        }
     }
 
     return (
@@ -45,10 +61,10 @@ export default function NavBar() {
                 />
             </Link>
             <ul className="flex space-x-4">
-                {items.map((items, index) => (
+                {items.map((item, index) => (
                     <li key={index}>
-                        <Link href={items.href} className="hover:text-zinc-400 transition-colors">
-                            {items.label}
+                        <Link href={item.href} className="hover:text-zinc-400 transition-colors">
+                            {item.label}
                         </Link>
                     </li>
                 ))}
