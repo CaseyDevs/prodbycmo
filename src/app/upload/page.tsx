2a25f3a619f365884be5afaabad46f4eaea8ed70
Casey Page <CaseyDevs5@gmail.com>
2025-05-23 22:13:58 +0100

    Redirect non admins to home page

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar/NavBar";
 import { FaCloudUploadAlt } from "react-icons/fa";
 import { redirect } from "next/navigation";
@@ -16,7 +16,21 @@ export default function Upload() {
   const [isUploading, setIsUploading] = useState(false);
   const [uploadProgress, setUploadProgress] = useState(0);
   const [fileSizeInMB, setFileSizeInMB] = useState(0);
-  const [error, setError] = useState<string | null>(null)
+  const [error, setError] = useState<string | null>(null);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    const role = localStorage.getItem("role");
+    if (role !== "ADMIN") {
+      redirect("/");
+    } else {
+      setChecked(true);
+    }
+  }, []);
+
+  if (!checked) {
+    return null;
+  }
 
   function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
     const selectedFile = event.target.files?.[0];
