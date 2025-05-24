f1ea04ccb5620769006fdfe1c1e40f4d5c90d637
Casey Page <CaseyDevs5@gmail.com>
2025-05-25 00:05:17 +0100

    Display beats on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,43 @@
+"use client";
+
 import NavBar from "../components/NavBar/NavBar"
+import { useEffect, useState } from "react";
+
+type Beat = {
+    id: string,
+    title: string,
+    genre: string,
+    bpm: number,
+    key: string,
+}
 
 export default function DashboardPage() {
+    const [beats, setBeats] = useState<Beat[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchBeats() {
+            try {
+                const response = await fetch("/api/songs");
+                if (!response.ok) {
+                    throw new Error("Failed to fetch beats");
+                }
+                const data = await response.json();
+                setBeats(data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching beats:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchBeats();
+    }, []);
+
+    if (loading) {
+        return <div className="flex items-center justify-center min-h-screen">Loading...</div>;
+    }
 
     return (
         <>
@@ -8,7 +45,23 @@ export default function DashboardPage() {
             <div className="flex flex-col items-center min-h-screen bg-gray-100 text-black">
                 <h1 className="text-4xl font-bold m-4">Dashboard</h1>
                 <p className="text-lg">Welcome Admin!</p>
-                <p className="text-lg">This is a placeholder page.</p>
+                <h3 className="text-lg">Your beats: </h3>
+                {beats.map((beat) => (
+                    <div key={beat.id} className="flex flex-row gap-4 bg-white shadow-md rounded-lg p-4 m-2 w-full max-w-xl">
+                        <img src={beat.coverImg} alt="Cover Img" className="h-20 w-20" />
+                        <div className="flex flex-col">
+                            <h2 className="text-xl font-semibold">{beat.title}</h2>
+                            <div className="flex flex-row gap-4">
+                                <p className="text-gray-600">Genre: {beat.genre}</p>
+                                <p className="text-gray-600">BPM: {beat.bpm}</p>
+                                <p className="text-gray-600">Key: {beat.key}</p>
+                            </div>
+                            <div>
+                                <button className="text-red-500 rounded-lg p-1 text-sm hover:cursor-pointer">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </>
     )
